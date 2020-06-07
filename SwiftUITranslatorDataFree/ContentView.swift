7309f19e8939010b99096268480893b7860d5cbb
dextronder97 <dexixoxa@yahoo.it>
2020-06-07 17:24:08 +0200

    Initial Commit

---
+++ SwiftUITranslatorDataFree/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITranslatorDataFree
+//
+//  Created by admin on 07/06/2020.
+//  Copyright © 2020 DenysPashkov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
